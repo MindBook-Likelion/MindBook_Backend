15a60f99da34c6d3ed61851dbe16ae29106ea100
baeksonghyun <bsh0374@gmail.com>
2024-07-29 18:53:34 +0900

    ✨ feat: 기쁨 회고록 닉네임 추가 개발

--- src/main/java/org/oa/mindbook/Service/MemoirComment/JoyMemoirCommentService.java
+++ src/main/java/org/oa/mindbook/Service/MemoirComment/JoyMemoirCommentService.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.oa.mindbook.Domain.Entity.Memoir.JoyMemoir;
 import org.oa.mindbook.Domain.Entity.MemoirComment.JoyMemoirComment;
+import org.oa.mindbook.Domain.Entity.User;
 import org.oa.mindbook.Dto.request.MemoirComment.CreateJoyMemoirCommentRequestDto;
 import org.oa.mindbook.Repository.Memoir.JoyMemoirRepository;
 import org.oa.mindbook.Repository.MemoirComment.JoyMemoirCommentRepository;
+import org.oa.mindbook.Repository.User.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,12 +19,14 @@ public class JoyMemoirCommentService {
 
     private final JoyMemoirCommentRepository joyMemoirCommentRepository;
     private final JoyMemoirRepository joyMemoirRepository;
+    private final UserRepository userRepository;
     @Transactional
     public void saveJoyMemoirComment(CreateJoyMemoirCommentRequestDto dto) {
         JoyMemoir joyMemoir = joyMemoirRepository.findById(dto.getJoyMemoirId()).orElseThrow();
+        User user = userRepository.findById(dto.getUserId()).orElseThrow();
 
         joyMemoirCommentRepository.save(JoyMemoirComment.builder()
-                .userId(dto.getUserId())
+                .user(user)
                 .joyMemoir(joyMemoir)
                 .content(dto.getContent())
                 .build());
