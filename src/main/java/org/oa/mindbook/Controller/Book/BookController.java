072f20ce24d57d9e58c10ed345e2db0679876862
noeyoes <lsy021128@naver.com>
2024-07-29 16:53:34 +0900

    ✨ feat: 유저별 추가된 도서 조회 기능 구현

--- src/main/java/org/oa/mindbook/Controller/Book/BookController.java
+++ src/main/java/org/oa/mindbook/Controller/Book/BookController.java
@@ -32,6 +32,6 @@ public class BookController {
     List<BookResDto> getBookList(@PathVariable Long userId) {
         List<BookResDto> bookResDto = bookService.findAllByUserId(userId);
 
-        return bookResDto
+        return bookResDto;
     }
 }
